7c8f46d45dc612988af2ac9a187bd3d3c2e2abc3
lokises <lokises@mcircle.biz>
2023-11-05 00:41:57 +0900

    중복 도서 체크

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,87 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import readXlsxFile from 'read-excel-file'
+import * as XLSX from 'xlsx';
 
-function App() {
+const App = () => {
+  const [excelJson, setExcelJson] = useState<any>([]);
+  const [studentList, setStudentList] = useState<any>([]);
+  const fileInput=useRef<any>(null);
+
+  const fileLoad = (e: any) => {
+    let input = e.target;
+    let reader = new FileReader();
+    reader.onload = function () {
+        let data = reader.result;
+        let workBook = XLSX.read(data, { type: 'binary' });
+        let index = 0;
+        let newBookList: any = [];
+
+        workBook.SheetNames.forEach(function (sheetName) {
+            let rows = XLSX.utils.sheet_to_json(workBook.Sheets[sheetName]);
+
+            rows.map((item: any, idx: number) => {
+              if(item.__EMPTY_1 !== undefined && item.__EMPTY_1 !== '이  름') {
+                let bookList = item.__EMPTY_5.split('), ');
+                bookList = bookList.map((item: string) => {
+                  if(item.split('').reverse()[0] !== ')') {
+                    return item + ')';
+                  } else {
+                    return item;
+                  }
+                });
+                newBookList.push({studentName: item.__EMPTY_1, bookList: bookList});
+                index++;
+              } else if(item.__EMPTY_1 === undefined && item.__EMPTY_5 !== undefined) {
+                let bookList = item.__EMPTY_5.split('), ');
+                bookList = bookList.map((item: string) => {
+                  if(item.split('').reverse()[0] !== ')') {
+                    return item + ')';
+                  } else {
+                    return item;
+                  }
+                });
+                newBookList[index-1].bookList.push(...bookList);
+              }
+            });
+        });
+        setExcelJson(newBookList);
+    };
+    reader.readAsBinaryString(input.files[0]);
+  }
+
+  const checkDuplication = (arr: any, studentName: string) => {
+    const result = arr.filter((item: any, index: number) => arr.indexOf(item) !== index);
+
+    if(result.length > 0) {
+      setStudentList((pre: any) => [...pre, {studentName: studentName, bookList: result}]);
+    }
+  }
+
+  useEffect(() => {
+    excelJson.map((item: any) => {
+      checkDuplication(item.bookList, item.studentName);
+    })
+  }, [excelJson])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <div>배포 테스트</div>
-      </header>
+      <input type='file' id={`file_upload`} ref={fileInput} onChange={(e) => {
+        fileLoad(e)
+      }}></input>
+      <button onClick={() => {
+        fileInput.current.value = "";
+        setExcelJson([]);
+        setStudentList([]);
+      }}>리셋</button>
+      <div>
+        <ul>
+          {studentList.map((item: any, idx: number) => {
+            return (<li key={idx}>
+              {item.studentName} : {item.bookList.join(', ')}
+            </li>)
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
