e223904b4a2f28775a3bfbcc64c53b32326a9ae1
lokises <lokises@mcircle.biz>
2023-11-12 22:46:21 +0900

    사용법, 주의사항 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 import readXlsxFile from 'read-excel-file'
 import * as XLSX from 'xlsx';
+import './style/app.scss';
 
 const App = () => {
   const [excelJson, setExcelJson] = useState<any>([]);
@@ -25,10 +26,11 @@ const App = () => {
               if(item.__EMPTY_1 !== undefined && item.__EMPTY_1 !== '이  름') {               
                 let bookList = item.__EMPTY_5.split('), ');
                 bookList = bookList.map((item: string) => {
-                  if(item.split('').reverse()[0] !== ')') {
-                    return item + ')';
+                  const newBook = item.replaceAll(/ /g, '');
+                  if(newBook.split('').reverse()[0] !== ')') {
+                    return newBook + ')';
                   } else {
-                    return item;
+                    return newBook;
                   }
                 });
                 if(changeWord.trim().length > 0) {
@@ -44,10 +46,11 @@ const App = () => {
               } else if(item.__EMPTY_1 === undefined && item.__EMPTY_5 !== undefined) {
                 let bookList = item.__EMPTY_5.split('), ');
                 bookList = bookList.map((item: string) => {
-                  if(item.split('').reverse()[0] !== ')') {
-                    return item + ')';
+                  const newBook = item.replaceAll(/ /g, '');
+                  if(newBook.split('').reverse()[0] !== ')') {
+                    return newBook + ')';
                   } else {
-                    return item;
+                    return newBook;
                   }
                 });
                 if(changeWord.trim().length > 0) {
@@ -100,7 +103,48 @@ const App = () => {
 
   return (
     <div className="App">
-      <input type='text' ref={wordRef}></input>
+      <div>
+        <h1>독서 활동 상황 중복 도서 체크</h1>
+        <h3>사용 방법</h3>
+        <ul>
+          <li>
+            공통 사용법
+            <ul>
+              <li>중복이 여러개일 경우 중복 횟수만큼 출력된다.</li>
+              <li>조금이라도 다른 철자가 있을 경우 중복으로 체크되지 않는다. (띄어쓰기 제외)</li>
+              <li>중복체크를 한번 진행한 후 리셋을 눌러 다른 파일을 첨부한다.</li>
+            </ul>
+          </li>
+          <br />
+          <li>
+            책 제목(지은이) 로 검색하기
+            <ul>
+              <li><strong>( 를 변경할 문자</strong> 를 입력하지 않고 파일 선택을 한다.</li>
+              <li>중복이 있을 경우 <strong>책제목(지은이)</strong> 의 형태로 출력된다.</li>
+            </ul>
+          </li>
+          <br />
+          <li>
+            책 제목 으로만 검색하기
+            <ul>
+              <li><strong>( 를 변경할 문자</strong> 를 입력하고 저장을 누른다.</li>
+              <li>변경할 문자를 먼저 입력하고 파일을 첨부한다.</li>
+              <li>변경할 문자로 띄어쓰기는 사용할 수 없다.</li>
+              <li>변경할 문자로 특수기호 및 여러글자를 입력할 수 있다. ex) #(가능), #$#wer(가능)</li>
+              <li>중복이 있을 경우 <strong>책제목</strong> 혹은 <strong>지은이)</strong> 의 형태로 출력된다.</li>
+              <li>책제목만 판단하고 <strong>지은이)</strong> 는 무시한다.</li>
+            </ul>
+          </li>
+          <br />
+          <li className='warning'>
+            ※주의사항※
+            <ul>
+              <li>엑셀에 중간에 책제목이 끊긴 칸이 있으면 해당 부분은 판단이 불가능하다.</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <input type='text' placeholder='( 를 변경할 문자' ref={wordRef}></input>
       <button onClick={() => {
         if(wordRef.current?.value) {
           setChangeWord(wordRef.current?.value);
